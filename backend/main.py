a826a00e47081968b99aa95b9be671b8d54b0e11
NTNVan <2212490@dlu.edu.vn>
2026-05-06 16:23:59 +0700

    feat: bai 10 - tao API don gian voi FastAPI

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Chao mung ban den voi Ebook2LateX!"}
+@app.get("/multiply/{number}")
+def multiply_number(number: int):
+    result = number * 10
+    return {"input": number, "result": result}
+@app.get("/buy-shoes")
+def buy_shoes(brand: str, size: int):
+    return {
+        "message": f"Bạn muốn mua giày {brand} kích thước {size} đúng không?"
+    }
